7f4b0a2d1c2f6f6b246ba6261252c3f573c1d867
Stefano Casciarri <s18449@pjwstk.edu.pl>
2021-02-14 13:40:54 +0100

    Egzamin

---
+++ Controllers/HealthcareController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using egzamin.Models;
+
+namespace egzamin.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class MyController : ControllerBase
+    {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s18449;Integrated Security=True";
+
+        [HttpGet]
+        public IActionResult GetStudent(string idMedicament)
+        {
+            var list = new List<Student>();
+
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from students";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Student st = new Student();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                }
+
+                return Ok(list);
+
+            }
+
+        }
+
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Services/SqlServerStudentDbService.cs
@@ -0,0 +1,118 @@
+﻿using egzamin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Wyklad5.Services
+{
+    public class SqlServerStudentDbService : IMedicineDbService
+    {
+
+        public Medicament GetMedicament(string idMedicament)
+        {
+            using (var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18449;Integrated Security=True"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+
+                con.Open();
+                var tran = con.BeginTransaction();
+
+                try
+                {
+
+                    com.CommandText = "select * from medicament where idmedicament = @idmedicament";
+                    com.Parameters.AddWithValue("idmedicament", idMedicament);
+
+                    var dr1 = com.ExecuteReader();
+
+                    if (!dr1.Read())
+                    {
+                        throw new HttpResponseException()
+                    }
+
+
+                }
+                catch (SqlException exc)
+                {
+                    tran.Rollback();
+                }
+            }
+
+            return Ok(ble);
+        }
+
+        public void DeletePatient(string idPatient)
+        {
+            var st = new Student();
+            st.FirstName = request.FirstName;
+
+            using (var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18449;Integrated Security=True"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+
+                con.Open();
+                var tran = con.BeginTransaction();
+
+                try
+                {
+
+                    com.CommandText = "select IdStudies from studies where indexnumber = @index";
+
+                    com.Parameters.AddWithValue("index", request.IndexNumber);
+
+                    var dr = com.ExecuteReader();
+
+                    if (dr.Read())
+                    {
+                        tran.Rollback();
+                        return BadRequest("Numer indeksu nie jest unikatowy");
+
+                    }
+
+                    com.CommandText = "select IdStudies from studies where name = @name";
+
+                    com.Parameters.AddWithValue("name", request.Studies);
+
+                    var dr1 = com.ExecuteReader();
+
+                    if (!dr1.Read())
+                    {
+                        tran.Rollback();
+                        return BadRequest("Studia nie istnieja");
+
+                    }
+
+                    int idstudies = (int)dr1["IdStudies"];
+
+                    com.CommandText = "INSERT INTO Student(IndexNumber, FirstName) VALUES(@Index, @Fname)";
+
+                    com.Parameters.AddWithValue("index", request.IndexNumber);
+
+                    com.ExecuteNonQuery();
+
+                    tran.Commit();
+
+                }
+                catch (SqlException exc)
+                {
+                    tran.Rollback();
+                }
+
+
+
+            }
+
+            throw new NotImplementedException();
+        }
+
+
+
+      
+    }
+}
